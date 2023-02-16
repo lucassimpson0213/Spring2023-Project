a94a3731e9291d8ec1aa73c57a8f9086536f0cd7
LoonyFrinkelboom <113646685+LoonyFrinkelboom@users.noreply.github.com>
2023-02-15 21:54:21 -0600

    Still incomplete

--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TransitionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Animator animator;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
     }
 }
