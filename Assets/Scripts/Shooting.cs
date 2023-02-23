6fc807bd635c1b2f8f60c291b37215117e25506c
Shanes444 <54292542+Shanes444@users.noreply.github.com>
2023-02-22 20:41:14 -0600

    finished shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -8,20 +8,23 @@ public class Shooting : MonoBehaviour
     public GameObject BulletPrefab;
 
     [SerializeField] float bulletForce;
-
+    [SerializeField] float bulletCooldown;
+    private bool canFire;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canFire = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
+            if (Input.GetButton("Fire1") && canFire)
+            {
+                Shoot();
+                canFire = false;
+                StartCoroutine(fireWait());
+            }
     }
     void Shoot()
     {
@@ -29,4 +32,11 @@ public class Shooting : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
     }
+    IEnumerator fireWait()
+    {
+        yield return new WaitForSeconds(bulletCooldown);
+        canFire = true;
+    }
 }
+
+
