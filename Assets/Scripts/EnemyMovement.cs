960b37d3d8f36ec7be05579e6edbccc55970f0a4
Shanes444 <54292542+Shanes444@users.noreply.github.com>
2023-02-22 21:58:47 -0600

    Completed Enemy Movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,10 +6,11 @@ public class EnemyMovement : MonoBehaviour
 {
     
 
-    public float speed;
+    [SerializeField] float speed;
     public GameObject point;
     Rigidbody2D rb;
     Vector2 pos;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,8 +20,13 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         //transform.position = Vector2.MoveTowards(transform.position, point.transform.position, speed * Time.deltaTime); 
-        pos = new Vector2(point.transform.position.x + rb.position.x, point.transform.position.y + rb.position.y);
+        //pos = new Vector2(point.transform.position.x + rb.position.x, point.transform.position.y + rb.position.y);
 
+        //rb.MovePosition(rb.position + pos.normalized * speed * Time.deltaTime);
+        
+        pos = point.transform.position - (Vector3)(rb.position);
+        float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
         rb.MovePosition(rb.position + pos.normalized * speed * Time.deltaTime);
     }
 }
