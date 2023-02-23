688188f4eeb80bfe0ce47148061c759c3f860212
Muntenberg <55027521+Muntenberg@users.noreply.github.com>
2023-02-23 10:03:04 -0600

    Finished the Enemy Attack Manager Script
    Will shoot a prefab at the nearest target with target priority

--- Assets/Scripts/EnemyAttacks/EnemyMeleeAttackManager.cs
+++ Assets/Scripts/EnemyAttacks/EnemyMeleeAttackManager.cs
@@ -5,35 +5,47 @@ using System.Linq;
 
 public class EnemyMeleeAttackManager : MonoBehaviour
 {
-    [SerializeField] GameObject meleeAttackPrefab;
     public float enemyAttackSpeed;
     private float lastAttack = 0;
     private List<Collider2D> collidersInside = new List<Collider2D>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(collidersInside.Count);
-        if (Time.time > lastAttack + enemyAttackSpeed)
+        // This function will fire a Prefab at the direction of a target within its collider.
+        // The target will depend on who is closer and what priority.
+        // Towers will be targeted first even though the player is closer.
+        // It will only attack the player if there is not towers in its collider.
+        // Targeting system will use the Tower and Player Tags.
+        // Attack target every n seconds.
+        if (Time.time > lastAttack + enemyAttackSpeed && collidersInside.Count > 0)
         {
             lastAttack = Time.time;
+            //sorts all of the objects in the array in order by distance.
             var target = collidersInside.OrderBy(go => (transform.position - go.transform.position).sqrMagnitude).ToList();
             if (target.Any(item => item.tag == "Tower"))
             {
-                //Debug.Log(target.IndexOf(target.Where(x => x.tag == "Tower").FirstOrDefault()));
-                Debug.Log(target[target.IndexOf(target.Where(x => x.tag == "Tower").FirstOrDefault())].transform.position);
+                // Gets the first object with the tag of Tower, no matter what postion it is from the array.
+                Vector3 vectorToTarget = target[target.IndexOf(target.Where(x => x.tag == "Tower").FirstOrDefault())].transform.position - transform.position;
+                // Gets the angle of the target from the fireing origin.
+                float targetAngle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+                GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0,0,targetAngle));
+                
+            } else
+            {
+                // Gets the first object with the tag of Player, no matter what postion it is from the array.
+                Vector3 vectorToTarget = target[target.IndexOf(target.Where(x => x.tag == "Player").FirstOrDefault())].transform.position - transform.position;
+                // Gets the angle of the target from the fireing origin.
+                float targetAngle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+                GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, targetAngle));
             }
         }
-
-        //Debug.Log(target[0].name);
     }
+    // Creates the array of objects inside of the collider.
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Checks if collider already exist inside of the array.
         if (!collidersInside.Contains(other))
         {
             collidersInside.Add(other);
