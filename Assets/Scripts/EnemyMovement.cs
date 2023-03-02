9e75fda2189e1fc683fab4eec9dddfd9912d6116
Shanes444 <54292542+Shanes444@users.noreply.github.com>
2023-03-01 20:34:06 -0600

    necessary push

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -17,16 +17,15 @@ public class EnemyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //transform.position = Vector2.MoveTowards(transform.position, point.transform.position, speed * Time.deltaTime); 
-        //pos = new Vector2(point.transform.position.x + rb.position.x, point.transform.position.y + rb.position.y);
 
-        //rb.MovePosition(rb.position + pos.normalized * speed * Time.deltaTime);
         
         pos = point.transform.position - (Vector3)(rb.position);
         float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
         rb.MovePosition(rb.position + pos.normalized * speed * Time.deltaTime);
+
+
     }
 }
